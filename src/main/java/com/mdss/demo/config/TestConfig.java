db568bb2a1b37eec1893db014365d17e8f2bd225
Marcelo Soares <marcelossoares@live.com>
2022-05-22 11:48:16 -0300

    OrderItem, ManyToMany association with extra attributes

--- src/main/java/com/mdss/demo/config/TestConfig.java
+++ src/main/java/com/mdss/demo/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.mdss.demo.entities.Category;
 import com.mdss.demo.entities.Order;
+import com.mdss.demo.entities.OrderItem;
 import com.mdss.demo.entities.Product;
 import com.mdss.demo.entities.User;
 import com.mdss.demo.entities.enums.OrderStatus;
 import com.mdss.demo.repositories.CategoryRepository;
+import com.mdss.demo.repositories.OrderItemRepository;
 import com.mdss.demo.repositories.OrderRepository;
 import com.mdss.demo.repositories.ProductRepository;
 import com.mdss.demo.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -68,6 +73,14 @@ public class TestConfig implements CommandLineRunner{
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 	}
 	
 	
