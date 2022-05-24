3f9d4b9761cdf9a8992fee29f7df756bdef553ca
Marcelo Soares <marcelossoares@live.com>
2022-05-24 20:40:01 -0300

    Database Exception

--- src/main/java/com/mdss/demo/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/mdss/demo/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.mdss.demo.services.exceptions.DatabaseException;
 import com.mdss.demo.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -19,6 +20,14 @@ public class ResourceExceptionHandler {
 			String error = "Resource not Found";
 			HttpStatus status = HttpStatus.NOT_FOUND;
 			StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+			return ResponseEntity.status(status).body(err);
+		}
+		
+		@ExceptionHandler(DatabaseException.class)
+		public ResponseEntity<StandardError> databaseException(DatabaseException e, HttpServletRequest request){
+			String erro = "Client has order";
+			HttpStatus status = HttpStatus.BAD_REQUEST;
+			StandardError err = new StandardError(Instant.now(),status.value(),erro, e.getMessage(), request.getRequestURI());
 			return ResponseEntity.status(status).body(err);
 		}
 }
